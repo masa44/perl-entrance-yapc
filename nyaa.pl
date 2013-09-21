cd3bd893510fbe7f8aae77ed20d16f959e2d2411
masa44 <mashumalow@gmail.com>
2013-09-21 14:06:07 +0900

    nyaa

--- nyaa.pl
+++ nyaa.pl
@@ -5,12 +5,8 @@ use strict;
 use warnings;
 
 use Acme::Nyaa;
+
 my $kijitora = Acme::Nyaa->new;
-
-#print $kijitora->cat( \'猫がかわいい。' );  # => 猫がかわいいニャー。
-#print $kijitora->neko( \'神と和解せよ' );   # => ネコと和解せよ
-
 my $input = <STDIN>;
 chomp $input;
-
 print $kijitora->cat(\$input);
